67a3f9d2cfbd89cd6a6c7b7044f75c9a3c02e598
Luis Larico <luisfernandolaricoticona@gmail.com>
2026-06-08 16:10:04 -0400

    Preparar variables de entorno para hosting

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,28 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bdsitec"
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "bdsitec",
+  ssl:
+    process.env.DB_SSL === "true"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : undefined,
 });
 
 conexion.connect((error) => {
-
-  if(error){
+  if (error) {
     console.log("Error BD:", error);
     return;
   }
 
   console.log("Base conectada correctamente");
-
 });
 
 module.exports = conexion;
