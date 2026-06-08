f362f303628850c3259b174a299499e2629a8be9
Luis Larico <luisfernandolaricoticona@gmail.com>
2026-06-08 19:25:43 -0400

    Corregir nombres de estilos  para Vercel

--- backend/server.js
+++ backend/server.js
@@ -99,8 +99,8 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor activo puerto ${PORT}`);
 });
